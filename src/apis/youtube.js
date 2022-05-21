b4db5d12fed6f8a7b66a64cda022fdab39b46123
Jason Le <jasonctle@gmail.com>
2022-05-21 12:35:24 -0700

    removed hard coded key, added env

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,13 +1,11 @@
 import axios from "axios";
 
-const KEY = "AIzaSyCwx2CtqOQRBn8RmvSexCp2L8O3In3MR18";
-
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
     part: "snippet",
     type: "video",
     maxResults: 5,
-    key: KEY,
+    key: `${process.env.REACT_APP_YOUTUBE_API_KEY}`,
   },
 });
